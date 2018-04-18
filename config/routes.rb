3131d472ee6fb15531cfa7e7a07c051a8b7ace1e
SaharakBanana <mju5804101398@gmaejo.mju.ac.th>
2018-04-18 16:56:03 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+  	root to: 'home#index'
+  end
+
+  unauthenticated :user do
+  	devise_scope :user do
+  		root to: 'devise/sessions#new'
+  		end
+  	end
+end
